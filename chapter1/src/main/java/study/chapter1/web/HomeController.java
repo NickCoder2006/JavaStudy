0b573d709808f73495d854c7f0fa3a78ca4719a5
NickMacWorkPC <zhengyanming2006@163.com>
2018-06-27 16:16:47 +0800

    Update home index show message.

--- chapter1/src/main/java/study/chapter1/web/HomeController.java
+++ chapter1/src/main/java/study/chapter1/web/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        return "This is homepage.";
+        return "This is homepage1.";
     }
 }
