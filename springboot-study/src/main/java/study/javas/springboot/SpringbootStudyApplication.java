f146aecd37b198d1ddbceb39a575574b0c17a39b
NickMacWorkPC <zhengyanming2006@163.com>
2018-07-06 10:22:37 +0800

    Package and deploy.

--- springboot-study/src/main/java/study/javas/springboot/SpringbootStudyApplication.java
+++ springboot-study/src/main/java/study/javas/springboot/SpringbootStudyApplication.java
@@ -3,12 +3,20 @@ package study.javas.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("study.javas.springboot.mapper")
-public class SpringbootStudyApplication {
+public class SpringbootStudyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootStudyApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
